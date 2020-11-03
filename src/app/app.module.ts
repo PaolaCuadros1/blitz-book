a96a12a757d5f8413f2fa3326143760059b8920c
PaolaCuadros1 <paola.cuadroso@gmail.com>
2020-11-03 17:59:59 -0500

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { MenuComponent } from './Components/menu/menu.component';
+import { FooterComponentComponent } from './Components/footer-component/footer-component.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, MenuComponent],
+  declarations: [AppComponent, HelloComponent, MenuComponent, FooterComponentComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
